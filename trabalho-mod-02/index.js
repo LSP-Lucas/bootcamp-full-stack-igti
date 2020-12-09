65e0377b648d8159d08ece3e2f5ee6df3658a2d8
Lucas Pedroso <lucassppedroso@gmail.com>
2020-12-09 16:50:14 -0300

    trabalho prático

--- trabalho-mod-02/index.js
+++ trabalho-mod-02/index.js
@@ -1,5 +1,7 @@
 import { promises as fs } from 'fs';
 
+let allStatesAndCity = [];
+
 stateAndCity();
 
 async function stateAndCity() {
@@ -10,11 +12,6 @@ async function stateAndCity() {
   const states = [];
   estados.forEach(estado => {
     const { ID, Sigla } = estado;
-    // const contais = citys[Sigla];
-
-    // if (!contais) {
-    //   citys[Sigla] = [];
-    // }
 
     states.push(Sigla);
 
@@ -25,22 +22,104 @@ async function stateAndCity() {
         citys.push(Nome)
       }
     });
-    fs.writeFile(`${Sigla}.json`, JSON.stringify(citys));
+
+    const objCity = {
+      cidades: citys
+    }
+
+    const arr = {
+      estado: Sigla,
+      cidade: citys
+    }
+
+    allStatesAndCity.push(arr);
+
+    fs.writeFile(`${Sigla}.json`, JSON.stringify(objCity));
   });
   readStates(states);
 }
 
 function readStates(states) {
+  console.log("\n\n2 - Total de cidades por estado em ordem crescente\n");
+
+  const arrBigStates = [];
+
   states.forEach(uf => {
-    fs.readFile("AC.json", "utf-8").then(resp => {
-      //console.log(`${uf}: ${resp.length}`);
-      console.log(uf + " " +resp)
+    fs.readFile(`${uf}.json`).then(resp => {
+      const value = JSON.parse(resp);
+      const totalCity = value.cidades.length;
+
+      console.log(`Estado: ${uf} - Toltal de cidades: ${totalCity}`);
+
+      const obj = {
+        Estado: `${uf}`,
+        Total: `${totalCity}`
+      }
+      arrBigStates.push(obj);
+
+      if (arrBigStates.length === states.length) {
+        bigStates(arrBigStates);
+      }
     }).catch(err => {
       console.log(err);
-    })
-    
-  })
+    });
+  });
 }
 
+function bigStates(arrBigStates) {
+  console.log("\n\n3 - Estados com maior quantidade de cidades em ordem decrescente\n");
+
+  const arrOrdenado = arrBigStates.sort(function (a, b) {
+    return a.Total - b.Total
+  });
+  let arrLenghtFive = [];
+  let newArrLenghtFive = [];
+  for (let i = 1; i <= 5; i++) {
+    arrLenghtFive.push(arrOrdenado[arrOrdenado.length - i]);
+  }
+
+  for (let value of arrLenghtFive) {
+    newArrLenghtFive.push(`${value.Estado} - ${value.Total}`)
+  }
+
+  console.log(newArrLenghtFive);
+  smallState(arrOrdenado);
+}
+
+function smallState(arrOrdenado) {
+  console.log("\n\n4 - Estados com maior quantidade de cidades em ordem decrescente\n");
+
+  let arrLenghtFive = [];
+  let newArrLenghtFive = [];
+
+  for (let i = 4; i >= 0; i--) {
+    arrLenghtFive.push(arrOrdenado[i]);
+  }
+
+  for (let value of arrLenghtFive) {
+    newArrLenghtFive.push(`${value.Estado} - ${value.Total}`)
+  }
+
+  console.log(newArrLenghtFive)
+  cityBigName();
+}
+
+function cityBigName() {
+  console.log("\n\n5 - Cidades com maior nome de cada estado\n");
+
+  const bigCity = [];
+  let bigNum = 0;
+
+  let varTemp = '';
+
+  for (let value of allStatesAndCity) {
+
+    value.cidade.forEach(valor => {
+      console.log(valor)
+    })
+
+  }
+
+}
 
 // Executa: node --experimental-modules index.js
