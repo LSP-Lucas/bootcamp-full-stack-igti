fb45de1091300a19bb6d39aae984f23bfee1023b
Lucas Pedroso <lucassppedroso@gmail.com>
2020-12-15 15:37:35 -0300

    instalação do winston e swagger

--- mod-02-my-bank-api/index.js
+++ mod-02-my-bank-api/index.js
@@ -1,29 +1,51 @@
 import express from 'express';
+import winston from 'winston';
+import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
 import { promises as fs } from 'fs';
+
 import accountsRouter from './routes/accounts.js';
+import { swaggerDocument } from './swagger.js';
 
 const { readFile, writeFile } = fs;
 
 global.fileName = "accounts.json";
 
+const { combine, timestamp, label, printf } = winston.format;
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+global.logger = winston.createLogger({
+  level: "silly",
+  transports: [
+    new (winston.transports.Console)(),
+    new (winston.transports.File)({ filename: "my-bank-api.log" })
+  ],
+  format: combine(
+    label({ label: "my-bank-api" }),
+    timestamp(),
+    myFormat
+  )
+});
+
 const app = express();
 app.use(express.json());
-
+app.use(cors());
+app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/account", accountsRouter);
-
 app.listen(3000, async () => {
   try {
     await readFile(global.fileName);
-    console.log("API Started!");
+    global.logger.info("API Started!");
   } catch (err) {
     const initialJson = {
       nextId: 1,
       accounts: []
     }
     writeFile(global.fileName, JSON.stringify(initialJson)).then(() => {
-      console.log("API Started and File Created!");
+      global.logger.info("API Started and File Created!");
     }).catch(err => {
-      console.log(err);
+      global.logger.error(err);
     });
   }
 });
