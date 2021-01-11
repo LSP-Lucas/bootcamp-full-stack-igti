919be9ae73d623e4af1b426b02b8964b985f624f
unknown <lucassppedroso@gmail.com>
2021-01-11 16:24:09 -0300

    React Lifecycle

---
+++ mod-react/react-lifecycle/src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import Toggle from './components/toggle/Toggle';
+import Users from './components/users/Users';
+
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      users: [],
+      showUsers: false
+    };
+  }
+
+  async componentDidMount() {
+    const res = await fetch('https://randomuser.me/api/?seed=rush&nat=br&results=10');
+
+    const json = await res.json();
+
+    this.setState({
+      users: json.results
+    })
+  }
+
+  handleShowUsers = (isChecked) => {
+    this.setState({ showUsers: isChecked });
+  }
+
+  render() {
+    const { showUsers, users } = this.state;
+    console.log(showUsers)
+
+    return (
+      <div>
+        <h3>React LifeCycle</h3>
+        <Toggle description="Mostrar usuários:" enable={showUsers} onToggle={this.handleShowUsers} />
+        <hr/>
+        {showUsers && <Users users={users} />}
+      </div>
+    );
+  }
+}
---
+++ mod-react/react-lifecycle/src/components/users/Users.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import User from './User';
+
+export default class Users extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      secondsVisible: 0
+    }
+
+    this.interval = null;
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount de Users.js");
+
+    this.interval = setInterval(() => {
+      const { secondsVisible } = this.state;
+
+      this.setState({
+        secondsVisible: secondsVisible + 1
+      })
+    }, 1000);
+  }
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate de Users.js");
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount de Users.js");
+
+    clearInterval(this.interval);
+  }
+  render() {
+    const { users } = this.props;
+    const { secondsVisible } = this.state;
+
+    return (
+      <div>
+        <p>Componente Users visivel por {secondsVisible} segundos </p>
+        <ul>
+          {users.map(user => {
+            const { login, name, picture } = user;
+            return (
+              <li key={login.uuid}><User user={user} /></li>
+            );
+          })}
+        </ul>
+      </div>
+    )
+  }
+}
